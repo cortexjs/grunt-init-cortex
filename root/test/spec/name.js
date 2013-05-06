0cec06f30692b3f61f74ba1d9b1f64082a8af513
supersheep <supersheepxjy@gmail.com>
2013-05-06 17:31:28 +0800

    init template

---
+++ root/test/spec/name.js
@@ -0,0 +1,6 @@
+NR.define(["src/{%= name %}"],function(NR,require){
+var {%= name %} = require("src/{%= name %}");
+describe("{%= name %}", function() {
+  
+});
+});
---
+++ template.js
@@ -0,0 +1,72 @@
+/*
+ * grunt-init-commonjs
+ * https://gruntjs.com/
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// Basic template description.
+exports.description = 'Create a neuron module, including unit tests.';
+
+// Template-specific notes to be displayed before question prompts.
+exports.notes = '';
+
+// Template-specific notes to be displayed after question prompts.
+exports.after = 'You should now install project dependencies with _npm ' +
+  'install_. After that, you may execute project tasks with _grunt_. For ' +
+  'more information about installing and configuring Grunt, please see ' +
+  'the Getting Started guide:' +
+  '\n\n' +
+  'http://gruntjs.com/getting-started';
+
+// Any existing file or directory matching this wildcard will cause a warning.
+exports.warnOn = '*';
+
+// The actual init template.
+exports.template = function(grunt, init, done) {
+
+  init.process({}, [
+    // Prompt for these values.
+    init.prompt('name'),
+    init.prompt('description'),
+    init.prompt('version'),
+    init.prompt('repository'),
+    init.prompt('homepage'),
+    init.prompt('bugs'),
+    init.prompt('licenses'),
+    init.prompt('author_name'),
+    init.prompt('author_email'),
+    init.prompt('node_version'),
+    init.prompt('npm_test', 'grunt test')
+  ], function(err, props) {
+    props.keywords = [];
+    props.devDependencies = {
+      "grunt": "~0.4.1",
+      "grunt-mocha": "~0.3.1",
+      "grunt-contrib-connect": "~0.3.0",
+      'grunt-contrib-concat': '~0.1.2',
+      'grunt-contrib-uglify': '~0.1.1',
+      'grunt-contrib-jshint': '~0.1.1',
+      'grunt-contrib-watch': '~0.2.0',
+    };
+
+    // Files to copy (and process).
+    var files = init.filesToCopy(props);
+
+    // Add properly-named license files.
+    init.addLicenseFiles(files, props.licenses);
+
+    // Actually copy (and process) files.
+    init.copyAndProcess(files, props);
+
+    // Generate package.json file.
+    init.writePackageJSON('package.json', props);
+
+    // All done!
+    done();
+  });
+
+};
