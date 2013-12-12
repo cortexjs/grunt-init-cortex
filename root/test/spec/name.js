5eca526cc09666546a16b29b1beff947d87fb007
kaelzhang <i@kael.me>
2013-12-13 00:09:09 +0800

    upgrade template to cortex@3.8.4

--- root/test/spec/name.js
+++ root/test/spec/name.js
@@ -1,8 +1,9 @@
 describe("{%= name %}", function(){
     describe("{%= name %}.my_method()", function(){
         it("should return 1", function(done){
-            NR.use(function() {
-                
+            _use('{%= name %}@latest', function(exports) {
+                expect('my_method' in exports);
+                done();
             });
         });
     });
